59760a68ab5538f4c5fb8d494cb13c5ad584957a
Silverlan <silverlann@gmail.com>
2022-11-25 20:10:34 +0100

    Remove obsolete library linking reference

--- src/wv_dmx.cpp
+++ src/wv_dmx.cpp
@@ -2,15 +2,6 @@
 #include <luainterface.hpp>
 #include <pragma/pragma_module.hpp>
 
-#pragma comment(lib,"lua51.lib")
-#pragma comment(lib,"luasystem.lib")
-#pragma comment(lib,"luabind.lib")
-#pragma comment(lib,"sharedutils.lib")
-#pragma comment(lib,"mathutil.lib")
-#pragma comment(lib,"vfilesystem.lib")
-#pragma comment(lib,"util_dmx.lib")
-#pragma comment(lib,"shared.lib")
-
 extern "C"
 {
 	void PRAGMA_EXPORT pragma_initialize_lua(Lua::Interface &l)
