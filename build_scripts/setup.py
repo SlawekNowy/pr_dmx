0b48a3701bc69ba74f6b781d1e53e8bdedaf2c27
SlawekNowy <38943477+SlawekNowy@users.noreply.github.com>
2024-02-26 13:49:15 +0100

    pr_source transitioned to setup script. So does this.

---
+++ build_scripts/setup.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+import sys
+import stat
+from sys import platform
+from pathlib import Path
+
+
+
+# util_dmx
+git_clone_commit(
+    "util_dmx",
+    external_libs_dir +"/util_dmx",
+    "https://github.com/Silverlan/util_dmx.git",
+    "0d7fca2"
+)
+
+
+cmake_args.append("-DPME_EXTERNAL_LIB_LOCATION=" +external_libs_dir)
+cmake_args.append("-DPME_EXTERNAL_LIB_BIN_LOCATION=" +external_libs_bin_dir)
+cmake_args.append("-DPME_THIRD_PARTY_LIB_LOCATION=" +third_party_libs_dir)
+
+additional_build_targets.append("pr_dmx")
